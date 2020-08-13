220c58b5dcf72486ed9bc1eadbd2478c8ce6b6d7
Cule219 <culaficstefan@gmail.com>
2020-08-12 21:14:11 -0400

    gql-jwt

---
+++ src/UserResolver.ts
@@ -0,0 +1,28 @@
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { hash } from "bcryptjs";
+import { User } from "./entity/User";
+
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return "hi!";
+  }
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email", () => String) email: string,
+    @Arg("password", () => String) password: string
+  ) {
+    const hashedPassword = await hash(password, 8);
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword,
+      });
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+}
---
+++ src/entity/User.ts
@@ -0,0 +1,13 @@
+import { Entity, ObjectIdColumn, ObjectID, Column, BaseEntity } from "typeorm";
+
+@Entity("users")
+export class User extends BaseEntity {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
